982565959469df86a4e4fc706c7f350819d887bb
andymason <a.w.mason@gmail.com>
2015-09-14 12:37:41 +0100

    Updated illustrations

--- src/views/svgs.js
+++ src/views/svgs.js
@@ -46,5 +46,6 @@ module.exports = {
 	urbanisation: '/imgs/illustrations/urbanisation.svg',
 	globalconference: '/imgs/illustrations/global-conference.svg',
 	millenniumgoals: '/imgs/illustrations/millennium-goals.svg',
+	poverty: '/imgs/illustrations/poverty.svg'
 };
 
